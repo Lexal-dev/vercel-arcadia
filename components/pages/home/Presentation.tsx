024949d77e2dd6767e4468ae4f91edaf7ef13d23
Lexal-dev <landoltalexis67@gmail.com>
2024-07-03 21:42:31 +0200

    fix error : &apos;

--- components/pages/home/Presentation.tsx
+++ components/pages/home/Presentation.tsx
@@ -6,7 +6,7 @@ export default function Presentation() {
       <section className="w-full flex flex-col gap-5 w-full md:w-2/3 lg:w-3/4 text-start ">
         <h1 className="font-bold font-sans text-center md:text-start text-2xl">Présentation du zoo</h1>
         <p className="text-start text-l">
-          Bienvenue au Zoo Arcadia, un lieu de merveilles et de découvertes. Notre zoo offre une expérience unique avec une variété d'animaux, de beaux paysages et des programmes éducatifs.
+          Bienvenue au Zoo Arcadia, un lieu de merveilles et de découvertes. Notre zoo offre une expérience unique avec une variété d&apos;animaux, de beaux paysages et des programmes éducatifs.
         </p>
         
         <div className="flex gap-6 justify-center">
