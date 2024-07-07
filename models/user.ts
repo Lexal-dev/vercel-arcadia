47c410536f4e48b9515387d688e03a4ccbf1eff2
Lexal-dev <landoltalexis67@gmail.com>
2024-07-07 17:57:27 +0200

    try to fix deployement

--- models/user.ts
+++ models/user.ts
@@ -1,5 +1,8 @@
 import { DataTypes, Model } from 'sequelize';
+import bcrypt from 'bcryptjs'; // Importez bcryptjs pour le hachage du mot de passe
 import sequelize from '@/lib/sequelize'; // Assurez-vous que votre configuration de base de données est correctement importée
+
+const SALT_ROUNDS = 10;
 
 interface UserAttributes {
   id?: number; // Rendre id optionnel
@@ -14,6 +17,16 @@ class User extends Model<UserAttributes> implements UserAttributes {
   public password!: string;
   public role!: string;
 
+  // Méthode pour hacher le mot de passe avant de sauvegarder
+  public async setPassword(password: string): Promise<void> {
+    const hashedPassword = await bcrypt.hash(password, SALT_ROUNDS);
+    this.password = hashedPassword;
+  }
+
+  // Méthode pour comparer un mot de passe non haché avec un mot de passe haché
+  public async comparePassword(password: string): Promise<boolean> {
+    return await bcrypt.compare(password, this.password);
+  }
 }
 
 User.init(
